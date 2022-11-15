41c6f223776efe025e19e9de6fb2c7ce19228039
NhatMinh <108985501+nhatminhdoan@users.noreply.github.com>
2022-11-15 22:00:25 +0700

    Update HomeLocators.java

--- src/test/java/Locators/HomeLocators.java
+++ src/test/java/Locators/HomeLocators.java
@@ -4,6 +4,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class HomeLocators {
-	 @FindBy(xpath = "//span[@class='oxd-topbar-header-breadcrumb']//h6[@class='oxd-text oxd-text--h6 oxd-topbar-header-breadcrumb-module']")
+	 @FindBy(xpath = "//h6[@class='oxd-text oxd-text--h6 oxd-topbar-header-breadcrumb-module']")
      public  WebElement homePageUserName;
 }
