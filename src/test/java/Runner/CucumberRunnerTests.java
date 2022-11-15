d81532f53c60c0a959851f313d588dd902e41254
NhatMinh <108985501+nhatminhdoan@users.noreply.github.com>
2022-11-15 22:05:33 +0700

    Update CucumberRunnerTests.java

--- src/test/java/Runner/CucumberRunnerTests.java
+++ src/test/java/Runner/CucumberRunnerTests.java
@@ -1,13 +1,13 @@
-package Runner;
+// package Runner;
 
-import org.junit.runner.RunWith;
+// import org.junit.runner.RunWith;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-@RunWith(Cucumber.class)
-@CucumberOptions(tags = "", features = "src/test/resources/features/LoginPage.feature", glue = "StepDefinitions",
-                 plugin = {})
+// import io.cucumber.junit.Cucumber;
+// import io.cucumber.junit.CucumberOptions;
+// @RunWith(Cucumber.class)
+// @CucumberOptions(tags = "", features = "src/test/resources/features/LoginPage.feature", glue = "StepDefinitions",
+//                  plugin = {})
     
-public class CucumberRunnerTests {
+// public class CucumberRunnerTests {
 
-}
+// }
